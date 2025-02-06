5022aaec911bd69f215834ccdf539d7ef77223a4
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-06 14:32:38 +0100

    refactoring code and creating other methods on the recipeView class

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,16 +4,23 @@ class RecipeView {
 
   render(data) {
     this.#data = data;
+    const markup = this.#generateMarkup;
+    this.#clear;
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  #clear() {
+    this.#parentElement.innerHTML = '';
   }
 
   #generateMarkup() {
     return `
           <figure class="recipe__fig">
-            <img src="${recipe.image}" alt="${
-      recipe.title
+            <img src="${this.#data.image}" alt="${
+      this.#data.title
     }" class="recipe__img" />
             <h1 class="recipe__title">
-              <span>${recipe.title}</span>
+              <span>${this.#data.title}</span>
             </h1>
           </figure>
 
@@ -23,7 +30,7 @@ class RecipeView {
                 <use href="${icons}#icon-clock"></use>
               </svg>
               <span class="recipe__info-data recipe__info-data--minutes">${
-                recipe.cookingTime
+                this.#data.cookingTime
               }</span>
               <span class="recipe__info-text">minutes</span>
             </div>
@@ -32,7 +39,7 @@ class RecipeView {
                 <use href="${icons}#icon-users"></use>
               </svg>
               <span class="recipe__info-data recipe__info-data--people">${
-                recipe.servings
+                this.#data.servings
               }</span>
               <span class="recipe__info-text">servings</span>
 
@@ -65,7 +72,7 @@ class RecipeView {
           <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-            ${recipe.ingredients
+            ${this.#data.ingredients
               .map(ing => {
                 return `
               <li class="recipe__ingredient">
@@ -101,13 +108,13 @@ class RecipeView {
             <p class="recipe__directions-text">
               This recipe was carefully designed and tested by
               <span class="recipe__publisher">${
-                recipe.publisher
+                this.#data.publisher
               }</span>. Please check out
               directions at their website.
             </p>
             <a
               class="btn--small recipe__btn"
-              href="${recipe.sourceUrl}"
+              href="${this.#data.sourceUrl}"
               target="_blank"
             >
               <span>Directions</span>
@@ -117,8 +124,6 @@ class RecipeView {
             </a>
           </div>
     `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   }
 }
 
