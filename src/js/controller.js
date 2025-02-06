effab236b901de76edc2f4e2b429b4b0601073cb
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-06 12:38:34 +0100

    creating and exporting the loadRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,26 +37,6 @@ const showRecipe = async function () {
     if (!id) return;
     // 1) Loading recipe
     renderSpiner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} ${data.status}`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
 
     // 2) Rendering recipe
     const markup = `
