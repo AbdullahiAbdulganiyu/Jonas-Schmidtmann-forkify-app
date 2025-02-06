93a509d800ba835d1071ae1a3c9eb7cb4933e52a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-06 15:37:25 +0100

    nicely formatting the quantity

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,6 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
+console.log(Fraction);
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
@@ -92,7 +94,9 @@ class RecipeView {
                 <svg class="recipe__icon">
                   <use href="src/img/icons.svg#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__quantity">${
+                  ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
