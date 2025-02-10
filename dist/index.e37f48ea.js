d344e0e237dabd7b4ccfd170078392f4750baad2
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-10 13:51:07 +0100

    making this._data.page a variable to avaoid repeatition

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3151,21 +3151,22 @@ var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class PaginationView extends (0, _viewJsDefault.default) {
     _parentElement = document.querySelector('.pagination');
     _generateMarkup() {
+        const curPage = this._data.page;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
         // Page 1 and there are other pages
-        if (this._data.page === 1 && numPages > 1) return 'Page 1';
+        if (curPage === 1 && numPages > 1) return 'Page 1';
         // Last page
-        if (this._data.page === numPages && numPages > 1) return `
+        if (curPage === numPages && numPages > 1) return `
         <button class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
               <use href="src/img/icons.svg#icon-arrow-left"></use>
             </svg>
-            <span>Page ${this._data.page - 1}</span>
+            <span>Page ${curPage - 1}</span>
         </button>
       `;
         // Other pages
-        if (this._data.page < numPages) return 'Other pages';
+        if (curPage < numPages) return 'Other pages';
         // Page 1 and there are no other pages
         return 'Only 1 page';
     }
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,30 +4,31 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const curPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
 
     // Page 1 and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
+    if (curPage === 1 && numPages > 1) {
       return 'Page 1';
     }
 
     // Last page
-    if (this._data.page === numPages && numPages > 1) {
+    if (curPage === numPages && numPages > 1) {
       return `
         <button class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
               <use href="src/img/icons.svg#icon-arrow-left"></use>
             </svg>
-            <span>Page ${this._data.page - 1}</span>
+            <span>Page ${curPage - 1}</span>
         </button>
       `;
     }
 
     // Other pages
-    if (this._data.page < numPages) {
+    if (curPage < numPages) {
       return 'Other pages';
     }
 
