fd292d859916c14bb6dd1905c18412e82fb802ba
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-10 10:31:50 +0100

    exporting new PaginationView

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,3 +3,5 @@ import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 }
+
+export default new PaginationView();
