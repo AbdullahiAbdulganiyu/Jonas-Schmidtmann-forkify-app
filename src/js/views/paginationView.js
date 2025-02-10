fcf6aa01cd8bf51c5acb4fec272d05f9d52583be
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-10 10:34:18 +0100

    craeting generateMarkup method on the paginationView class

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -2,6 +2,8 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {}
 }
 
 export default new PaginationView();
