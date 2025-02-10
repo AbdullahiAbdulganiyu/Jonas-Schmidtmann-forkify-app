7825cc323cfee7fbf7edff1121f81c879eb1a2e8
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-10 10:13:53 +0100

    getting the page number and adding it to the state

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2568,6 +2568,7 @@ const loadSearchResult = async function(query) {
     }
 };
 const getSearchResultsPage = function(page) {
+    state.search.page = page;
     const start = (page - 1) * state.search.resultsPerPage;
     const end = page * state.search.resultsPerPage;
     return state.search.results.slice(start, end);
