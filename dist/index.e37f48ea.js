abb2cec76da7a5a12b462385ef1db9988e9a2ecb
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-11 11:07:13 +0100

    fixing a bug caused by destructuring the btn.dataset

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2664,7 +2664,7 @@ class RecipeView extends (0, _viewJsDefault.default) {
         this._parentElement.addEventListener('click', function(e) {
             const btn = e.target.closest('.btn--update-servings');
             if (!btn) return;
-            const { updateTo } = +btn.dataset;
+            const updateTo = +btn.dataset.updateTo;
             handler(updateTo);
             console.log(btn);
         });
