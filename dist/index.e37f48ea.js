6dd03c8c2d6079737402ee0f86c72600b78665f6
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-12 21:03:23 +0100

    commenting out

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3209,25 +3209,6 @@ class ReultsView extends (0, _viewDefault.default) {
     _parentElement = document.querySelector('.results');
     _errorMessage = 'No recipes found for your query. Please try again :)';
     _message = '';
-    _generateMarkup() {
-        return this._data.map(this._generateMarkupPreview).join('');
-    }
-    _generateMarkupPreview(result) {
-        const id = window.location.hash.slice(1);
-        return `
-        <li class="preview">
-            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}"  href="#${result.id}">
-            <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-            </figure>
-            <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-            </div>
-            </a>
-        </li>
-    `;
-    }
 }
 exports.default = new ReultsView();
 
