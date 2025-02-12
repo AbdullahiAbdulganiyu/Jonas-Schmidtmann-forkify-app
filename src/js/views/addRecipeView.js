340016365f40121827a75b53bfb1edc538f80ab3
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-12 23:28:59 +0100

    selecting the upload element as the parent

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 class AddRecipeView extends View {
-  _parentElement = document.querySelector('.pagination');
+  _parentElement = document.querySelector('.upload');
 
   _generateMarkup() {}
 }
