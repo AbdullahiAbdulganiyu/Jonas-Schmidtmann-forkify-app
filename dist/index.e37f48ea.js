f331db99e69cc8c124c706516b4e124690eba0bd
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-14 11:07:01 +0100

    triming every array element insted of just replacing space

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2085,7 +2085,8 @@ const uploadRecipe = async function(newRecipe) {
         // creating an array of ingredient recieved from the newRecipe
         console.log(newRecipe);
         const ingredients = Object.entries(newRecipe).filter((entry)=>entry[0].startsWith('ingredient') && entry[1] !== '').map((ing)=>{
-            const ingArr = ing[1].replaceAll(' ', '').split(',');
+            const ingArr = ing[1].split(',').map((el)=>el.trim());
+            // const ingArr = ing[1].replaceAll(' ', '').split(',');
             if (ingArr.length !== 3) throw new Error('Wrong ingredient format. Please use the proper format :)');
             const [quantity, unit, description] = ingArr;
             return {
